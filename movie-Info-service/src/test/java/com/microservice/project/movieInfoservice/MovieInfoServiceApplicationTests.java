a5da262946657297845fcd549b9167d56b114c47
Satyajeet Prakash <sant8430@gmail.com>
2022-11-08 00:10:27 +0530

    Intial 3 Spring services

---
+++ movie-Info-service/src/test/java/com/microservice/project/movieInfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.project.movieInfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
